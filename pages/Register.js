da7b781142bd4a199246b639980496f8ce56a1f6
PrashantAmoli <prashantamoli@outlook.com>
2022-05-20 15:11:25 +0530

    Metamask phase1

--- pages/Register.js
+++ pages/Register.js
@@ -1,9 +1,19 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import Navbar from '../components/Navbar.js';
 import PatientRegistrationForm from '../components/forms/PatientRegistrationForm.js';
 import DoctorRegistrationForm from '../components/forms/DoctorRegistrationForm.js';
 
 export default function Register() {
+  const [Form, setForm] = useState(true);
+
+  const patientForm = () => {
+    setForm(true);
+  };
+  const doctorForm = () => {
+    setForm(false);
+  };
+
   return (
     <>
       <Head>
@@ -11,8 +21,15 @@ export default function Register() {
       </Head>
       <Navbar />
       <div>
-        <PatientRegistrationForm />
-        <DoctorRegistrationForm />
+        {Form === true ? (
+          <PatientRegistrationForm />
+        ) : (
+          <DoctorRegistrationForm />
+        )}
+      </div>
+      <div>
+        <button onClick={patientForm}>Patient Registration</button>
+        <button onClick={doctorForm}>Doctor Registration</button>
       </div>
     </>
   );
