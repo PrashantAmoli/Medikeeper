539ce6baa0e7d92b75925c62a97146b4c2134dfd
PrashantAmoli <prashantamoli@outlook.com>
2022-06-23 01:37:51 +0530

    Production v1.3

--- components/cards/Welcome.js
+++ components/cards/Welcome.js
@@ -1,18 +1,21 @@
+import styles from '../../styles/cards.module.css';
+
 export default function Welcome() {
   return (
     <>
-      <h1>Welcome to Medikeeper</h1>
-      <h3>Blockchain based medical record storage</h3>
-      <div>
-        <h5>
+      <div className={styles.card} style={{ boxShadow: 'none' }}>
+        <h1 className={styles.head}>Welcome to Medikeeper</h1>
+        <h3 className={styles.head}>Blockchain based medical record storage</h3>
+        <br />
+        <h5 className={styles.head}>
           Too often our health must take the back seat for what really is
           important,
         </h5>
-        <h5>
+        <h5 className={styles.head}>
           it is only when you lose your health that you realise how important it
           is.
         </h5>
-        <h5>
+        <h5 className={styles.head}>
           We are in the business of getting people's health back and teaching
           them how to stay healthy.
         </h5>
