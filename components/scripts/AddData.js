77f701235ae9a791443b46199a15e5877e5b349e
PrashantAmoli <prashantamoli@outlook.com>
2022-10-07 12:09:49 +0530

    Comments++

--- components/scripts/AddData.js
+++ components/scripts/AddData.js
@@ -122,7 +122,9 @@ const AddData = () => {
         (err, result) => {
           if (err) {
             //show modal here displaying the error that was given by the EVM compiler
-            alert('Not Authorised! Sorry, Become a doctor!!');
+            alert(
+              'Not Authorised! Sorry, Become a doctor to access patients records!!'
+            );
             console.log(err);
           } else if (result) console.log(result, JSON.stringify(result));
         }
@@ -134,124 +136,3 @@ const AddData = () => {
 };
 
 export default AddData;
-
-// import Web3 from 'web3';
-// import AddMedicalInfo from '../ABIs/AddMedicalInfo.json';
-// import { useState, useEffect } from 'react';
-
-// const AddMedicalInfoABI = AddMedicalInfo.abi;
-// const contractAddress = '0xB179ab9d325DFc545CfB69fC136Ef395f60482cb'; // Goerli
-// // const contractAddress = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'; // localhost
-
-// const AddData = () => {
-//   const [Account, setAccount] = useState('');
-
-//   const loadWeb3 = async () => {
-//     if (window.ethereum) {
-//       window.web3 = new Web3(window.ethereum);
-//       window.ethereum.enable();
-//     }
-//   };
-//   const load = async () => {
-//     await loadWeb3();
-//   };
-//   const getCurrentAccount = async () => {
-//     const accounts = await window.web3.eth.getAccounts();
-//     setAccount(accounts[0]);
-//     return accounts[0];
-//   };
-
-//   const addDoctor = async (Doctor) => {
-//     await load();
-
-//     const account = await getCurrentAccount();
-//     const contract = await new web3.eth.Contract(
-//       AddMedicalInfoABI,
-//       contractAddress
-//     );
-
-//     const result = await contract.methods
-//       .addMedicalInfoDoctor(
-//         Doctor.doctorsName,
-//         Doctor.doctorsID,
-//         Doctor.speciality,
-//         Doctor.hospital,
-//         Doctor.gender
-//       )
-//       .send(
-//         {
-//           from: account,
-//         },
-//         (err, result) => {
-//           if (err) console.log(err);
-//           else if (result) console.log(result);
-//         }
-//       );
-//     return result;
-//   };
-
-//   const addPatient = async (Patient) => {
-//     await load();
-
-//     const account = await getCurrentAccount();
-//     const contract = await new web3.eth.Contract(
-//       AddMedicalInfoABI,
-//       contractAddress
-//     );
-
-//     const result = await contract.methods
-//       .addMedicalInfoPatient(
-//         Patient.patientsName,
-//         Patient.patientsID,
-//         Patient.number,
-//         Patient.gender,
-//         Patient.address,
-//         Patient.dob,
-//         Patient.allergies
-//       )
-//       .send(
-//         {
-//           from: account,
-//         },
-//         (err, result) => {
-//           if (err) console.log(err);
-//           else if (result) console.log(result);
-//         }
-//       );
-//     return result;
-//   };
-
-//   const addRecord = async (Report) => {
-//     await load();
-
-//     const account = await getCurrentAccount();
-//     const contract = await new web3.eth.Contract(
-//       AddMedicalInfoABI,
-//       contractAddress
-//     );
-
-//     const result = await contract.methods
-//       .addMedicalRecords(
-//         Report.patientsID,
-//         Report.lastUpdated,
-//         Report.currentMedicalDosage,
-//         Report.updatedBy,
-//         Report.diagnosis,
-//         Report.pdf
-//       )
-//       .send(
-//         {
-//           from: account,
-//         },
-//         (err, result) => {
-//           if (err) console.log(err);
-//           else if (result) console.log(result);
-//         }
-//       );
-//     return result;
-//   };
-
-//   return { Account, addDoctor, addPatient, addRecord };
-// };
-
-// export default AddData;
--- components/scripts/Admin.js
+++ components/scripts/Admin.js
@@ -34,10 +34,6 @@ const Admin = () => {
       AddMedicalInfoABI,
       addContractAddress
     );
-    // const getAuth = await new web3.eth.Contract(
-    //   GetMedicalInfoABI,
-    //   getContractAddress
-    // );
     web3.utils.checkAddressChecksum(doctorAddress);
     web3.utils.isAddress(doctorAddress);
     console.log(doctorAddress);
@@ -57,23 +53,6 @@ const Admin = () => {
         }
       );
 
-    // const resultGet = await getAuth.methods
-    //   .addAuthorisedDoctor(doctorAddress)
-    //   .send(
-    //     {
-    //       from: account,
-    //     },
-    //     function (err, resultGet) {
-    //       if (err) {
-    //         console.log(err);
-    //       }
-    //       if (resultGet) {
-    //         console.log(resultGet);
-    //       }
-    //     }
-    //   );
-  }
-
   //remove Authentication, Remove from post of Doctor.
   async function removeAuthentication(removeAddressDoctor) {
     load();
@@ -85,10 +64,6 @@ const Admin = () => {
       AddMedicalInfoABI,
       addContractAddress
     );
-    // const getAuth = await new web3.eth.Contract(
-    //   GetMedicalInfoABI,
-    //   getContractAddress
-    // );
     const resultAdd = await addAuth.methods
       .removeAuthorisedDoctor(removeAddressDoctor)
       .send(
@@ -103,143 +78,9 @@ const Admin = () => {
           }
         }
       );
-
-    // const resultGet = await getAuth.methods
-    //   .removeAuthorisedDoctor(removeAddressDoctor)
-    //   .send(
-    //     {
-    //       from: account,
-    //     },
-    //     function (err, resultGet) {
-    //       if (err) {
-    //         console.log(err);
-    //       }
-    //       if (resultGet) console.log(resultGet);
-    //     }
-    //   );
   }
 
   return { addAuthentication, removeAuthentication };
 };
 
 export default Admin;
-
-// import Web3 from 'web3';
-// import { useState } from 'react';
-// import AddMedicalInfo from '../ABIs/AddMedicalInfo.json';
-// import GetMedicalInfo from '../ABIs/GetMedicalInfo.json';
-
-// const AddMedicalInfoABI = AddMedicalInfo.abi;
-// const GetMedicalInfoABI = GetMedicalInfo.abi;
-// const addContractAddress = '0xB179ab9d325DFc545CfB69fC136Ef395f60482cb'; // Goerli
-// const getContractAddress = '0x19b1970917E1Ec7D627924B65aaD0F4eac3BaD3b'; // Goerli
-
-// const Admin = () => {
-//   const [Account, setAccount] = useState('');
-
-//   const loadWeb3 = async () => {
-//     if (window.ethereum) {
-//       window.web3 = new Web3(window.ethereum);
-//       window.ethereum.enable();
-//     }
-//   };
-//   const load = async () => {
-//     await loadWeb3();
-//   };
-//   const getCurrentAccount = async () => {
-//     const accounts = await window?.web3?.eth?.getAccounts();
-//     await setAccount(accounts[0]);
-//     return accounts[0];
-//   };
-
-//   //Add Authentication , Add As Doctor
-//   async function addAuthentication(doctorAddress) {
-//     await load();
-//     const account = await getCurrentAccount();
-//     const addAuth = await new web3.eth.Contract(
-//       AddMedicalInfoABI,
-//       addContractAddress
-//     );
-//     const getAuth = await new web3.eth.Contract(
-//       GetMedicalInfoABI,
-//       getContractAddress
-//     );
-//     web3.utils.checkAddressChecksum(doctorAddress);
-//     web3.utils.isAddress(doctorAddress);
-//     console.log(doctorAddress);
-//     const resultAdd = await addAuth.methods
-//       .addAuthorisedDoctor(doctorAddress)
-//       .send(
-//         {
-//           from: account,
-//         },
-//         function (err, resultAdd) {
-//           if (err) {
-//             console.log(err);
-//           }
-//           if (resultAdd) console.log(resultAdd);
-//         }
-//       );
-
-//     const resultGet = await getAuth.methods
-//       .addAuthorisedDoctor(doctorAddress)
-//       .send(
-//         {
-//           from: account,
-//         },
-//         function (err, resultGet) {
-//           if (err) {
-//             console.log(err);
-//           }
-//           if (resultGet) {
-//             console.log(resultGet);
-//           }
-//         }
-//       );
-//   }
-
-//   //remove Authentication, Remove from post of Doctor.
-//   async function removeAuthentication(doctorAddress) {
-//     await load();
-//     const account = await getCurrentAccount();
-
-//     const addAuth = await new web3.eth.Contract(
-//       AddMedicalInfoABI,
-//       addContractAddress
-//     );
-//     const getAuth = await new web3.eth.Contract(
-//       GetMedicalInfoABI,
-//       getContractAddress
-//     );
-//     const resultAdd = await addAuth.methods
-//       .removeAuthorisedDoctor(doctorAddress)
-//       .send(
-//         {
-//           from: account,
-//         },
-//         function (err, resultAdd) {
-//           if (err) {
-//             console.log(err);
-//           }
-//         }
-//       );
-
-//     const resultGet = await getAuth.methods
-//       .removeAuthorisedDoctor(doctorAddress)
-//       .send(
-//         {
-//           from: account,
-//         },
-//         function (err, resultGet) {
-//           if (err) {
-//             console.log(err);
-//           }
-//           if (resultGet) console.log(resultGet);
-//         }
-//       );
-//   }
-
-//   return { addAuthentication, removeAuthentication };
-// };
-
-// export default Admin;
